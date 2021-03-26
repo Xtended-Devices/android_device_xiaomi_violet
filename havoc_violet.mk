5c14132992e99d1e299c75b903ff3700714ac062
Logan <athuldineshan5@gmail.com>
2021-03-26 14:11:27 +0530

    violet: Don't build Live Wallpapers and GApps by default

--- havoc_violet.mk
+++ havoc_violet.mk
@@ -13,8 +13,7 @@ $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 
-WITH_GAPPS=true
-TARGET_GAPPS_ARCH=arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := havoc_violet
