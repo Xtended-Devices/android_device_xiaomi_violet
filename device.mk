a9850c5c86e9a9f5992fbebb66fdc56bc387a118
abhishekpeddada <abhishekpeddada@gmail.com>
2022-12-19 18:12:00 +0530

    violet:XtendedOs Official Bringup

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/xiaomi/violet/violet-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
--- xtended_violet.mk
+++ xtended_violet.mk
@@ -6,8 +6,8 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xtended OS stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit ANX Camera
 #$(call inherit-product, vendor/ANXCamera/config.mk)
@@ -22,11 +22,15 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := xtended_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Xtended Stuffs
+XTENDED_BUILD_MAINTAINER := abhix202
+XTENDED_BUILD_TYPE := OFFICIAL
 
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
